5391aef7905afca94aed9ae48afc7f12e47678fe
jiejia2009 <jiejia2009@gmail.com>
2025-12-01 12:06:14 +0800

    Remove redundant "prose-sm" class and standardize text size for content containers

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -4,7 +4,7 @@
       <h4 class="text-md font-bold">{!! $title !!}</h4>
       @include('partials.entry-meta')
     </div>
-    <div class="max-w-none text-sm text-base-content/80 [&_p]:py-2 [&_img]:mx-auto [&_img]:cursor-pointer [&_img]:rounded-lg [&_img]:shadow-md" id="content">
+    <div class="max-w-none text-base text-base-content/80 [&_p]:py-2 [&_img]:mx-auto [&_img]:cursor-pointer [&_img]:rounded-lg [&_img]:shadow-md" id="content">
       @php(the_content())
     </div>
     @include('partials.entry-tags')
