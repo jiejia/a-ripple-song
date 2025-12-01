b03acb815364258af4a46eafbb296e22788c509d
jiejia2009 <jiejia2009@gmail.com>
2025-12-01 10:55:20 +0800

    Refactor player component styles for improved responsiveness and layout

--- resources/views/sections/player.blade.php
+++ resources/views/sections/player.blade.php
@@ -1,21 +1,21 @@
 <div class="card md:bg-base-100 bg-base-300/90 md:static fixed bottom-0 left-0 right-0 z-100" x-data>
-    <div class="card-body p-4">
-        <h2 class="text-lg font-bold">{!! __('NOW PLAYING', 'sage') !!}</h2>
-        <div class="grid grid-cols-[60px_1fr] gap-4 items-center md:bg-base-300/50 bg-base-100/75 p-4 rounded-lg">
-            <div class="w-15 h-15">
+    <div class="card-body md:p-4 py-2 px-4">
+        <h2 class="md:text-lg text-md font-bold">{!! __('NOW PLAYING', 'sage') !!}</h2>
+        <div class="grid grid-cols-[60px_1fr] gap-4 items-center md:bg-base-300/50 bg-base-100/75 md:p-4 py-2 px-4 rounded-lg">
+            <div class="md:w-15 md:h-15 w-10 h-10">
                 <template x-if="$store.player.currentEpisode?.featuredImage">
-                    <div class="relative w-15 h-15">
+                    <div class="relative md:w-15 md:h-15 w-10 h-10">
                         <img 
                             :src="$store.player.currentEpisode?.featuredImage" 
                             :alt="$store.player.currentEpisode?.title || '{{ __('Podcast', 'sage') }}'" 
-                            class="w-15 h-15 rounded-md object-cover" />
+                            class="md:w-15 md:h-15 w-10 h-10 rounded-md object-cover" />
                         <div class="pointer-events-none absolute inset-0 bg-base-900/30 flex items-center justify-center rounded-md">
                             <i data-lucide="podcast" class="w-6 h-6 text-base-100"></i>
                         </div>
                     </div>
                 </template>
                 <template x-if="!$store.player.currentEpisode?.featuredImage">
-                    <div class="w-15 h-15 rounded-md bg-base-300/60 flex items-center justify-center">
+                    <div class="md:w-15 md:h-15 w-10 h-10 rounded-md bg-base-300/60 flex items-center justify-center">
                         <i data-lucide="podcast" class="w-6 h-6 text-base-content/70"></i>
                     </div>
                 </template>
@@ -31,7 +31,7 @@
             </div>
         </div>
         <div>
-            <div class="h-[40px]" id="wave">
+            <div class="md:h-[40px] h-[30px]" id="wave">
             </div>
             <div class="mt-0 w-full">
                 <div class="grid grid-cols-[30px_1fr_30px] gap-2 items-center text-xs">
@@ -40,7 +40,7 @@
                     <span class="justify-self-end" x-text="$store.player.durationText">00:00</span>
                 </div>
             </div>
-            <div class="mt-2 grid grid-cols-[1fr_1fr_1fr] gap-4 items-center w-full">
+            <div class="mt-1 md:mt-2 grid grid-cols-[1fr_1fr_1fr] gap-4 items-center w-full">
                 <div class="flex items-center gap-2">
                     <label for="playlist-drawer" class="cursor-pointer">
                         <i data-lucide="list-music" class="w-4 h-4"></i>
