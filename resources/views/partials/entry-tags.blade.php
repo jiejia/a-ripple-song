7e0773475a0c331c5054c019955bb2b0d89c3dc3
jiejia <jiejia2009@gmail.com>
2026-01-01 15:58:14 +0800

    refactor: update tag list to use flex wrap for better spacing

--- resources/views/partials/entry-tags.blade.php
+++ resources/views/partials/entry-tags.blade.php
@@ -4,7 +4,7 @@
 
 @if($tags && !empty($tags))
 <div class="grid grid-flow-row gap-2 mt-2">
-    <ul class="flex gap-2 justify-center">
+    <ul class="flex flex-wrap gap-2">
         @foreach($tags as $tag)
         <li>
             <a href="{{ get_tag_link($tag->term_id) }}" class="text-xs text-base-content/50 bg-base-200/50 hover:bg-base-200 rounded-full py-0.5 px-2"># {{ $tag->name }}</a>
